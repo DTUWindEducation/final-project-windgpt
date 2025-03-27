7a60a00b3fe6e6beec9672d056d74be0e9ede21d
Thomas Røder <s204377@dtu.dk>
2025-03-27 12:04:47 +0100

    Added __init__.py with function to load input data

---
+++ src/__init__.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def load_data(file_path):
+    return pd.read_csv(file_path, index_col=0, parse_dates=True)
+
+FILE_PATH = './inputs/Location1.csv'
+data = load_data(FILE_PATH)
+data.plot(subplots=True)
+
+def scale_data(data):
+    return None
+
+
+def train_test_split(number_hours_input, number_hours_output, data):
+    return None
+
+
+def fit_model(model, Xtrain, ytrain):
+    return None
+
+
+def cross_validation(model, Xtrain, ytrain):
+    return None
+
+
+def predict(model, Xtest):
+    return None
+
